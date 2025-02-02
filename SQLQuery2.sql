06c77cc1754cad47cf63f251767a67437a862b16
RYAN KEE WEI YANG 2300326B <2300326B@student.tp.edu.sg>
2025-02-03 02:10:57 +0800

    Added User to Arguement and some visual changes.

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,13 +1,2 @@
 SELECT * 
-FROM Argument
-
-
-UPDATE Argument
-SET OnlineDebateUserId = NULL
-WHERE OnlineDebateUserId NOT IN (SELECT Id FROM AspNetUsers);
-
-ALTER TABLE Argument
-ALTER COLUMN OnlineDebateUserId nvarchar(450) NULL;
-
-UPDATE Argument
-SET OnlineDebateUserId = '3781efa7-66dc-47f0-860f-e506d04102e4'
+FROM Topic;
